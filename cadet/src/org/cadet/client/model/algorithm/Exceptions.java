fbb9751c01121f9aaa2fc280fc18d660ac4ab428
da201112014 <da201112014@gmail.com>
2012-10-11 00:01:22 +0000

    Algorithm files

---
+++ cadet/src/org/cadet/client/model/algorithm/Exceptions.java
@@ -0,0 +1,16 @@
+package org.cadet.client.model.algorithm;
+
+/**
+ *
+ * @author Jay Jobanputra
+ */
+public class Exceptions {
+    final static String zero_stepsize = "difference between difficulties is 0";
+    final static String zero_questions = "Number of questions in test cannot be 0 or less than 0";
+    final static String zero_maxdifficulty = "Maximum difficulty in test cannot be 0 or less than 0";
+    final static String negative_mindifficulty = "Minimum difficulty in test cannot be less than 0";
+    final static String mindifficulty_equals_maxdifficulty = "Minimum difficulty in test cannot be greater or equal to maximum difficulty";
+    final static String difficulty_exceeds_maxdifficulty = "Difficulty exceeds max_difficulty";
+    final static String difficulty_lower_mindifficulty = "Difficulty is lower than min_difficulty";
+    final static String difficulty_not_in_stepsize = "Difficulty can only be multiple of step size";
+}
