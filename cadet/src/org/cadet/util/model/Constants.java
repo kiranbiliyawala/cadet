6d742ed39765d6fde41fc44074049a93ddb8ed3a
da201112014 <da201112014@gmail.com>
2012-10-14 15:10:15 +0000

    Security Authentication - Jay 14 oct 2012 8:39 PM

--- cadet/src/org/cadet/util/model/Constants.java
+++ cadet/src/org/cadet/util/model/Constants.java
@@ -5,6 +5,15 @@ public class Constants {
 	public static final String logfile = "logfile.log";
 	
 	
+	public static final class Captcha{
+		
+		public static String VerifyUrl = "http://www.google.com/recaptcha/api/verify";
+		public static String publicKey = "6LeOsdcSAAAAAOHjj6wziFhEdSzLzxCQDjPbYPXo";
+		public static String privateKey = "6LeOsdcSAAAAAP3OwUfT_kxNXh5r9oJwvG3H4uSS";
+		
+	}
+	
+	
 	public static final class DB{
 		public static final String driver = "com.mysql.jdbc.Driver";
 		public static final String dburl = "jdbc:mysql://localhost:3306/cadet";
@@ -16,6 +25,7 @@ public class Constants {
 	public static final class sqlCommands{
 		public static final String retrivePasswordAdmin = "SELECT Password FROM admin WHERE Username = "; 
 		public static final String retrivePasswordClient = "SELECT Password FROM user WHERE Username = "; 
+		public static final String isClientAvailable = "SELECT COUNT(DISTINCT Username) AS count FROM user WHERE Username = ";
 		public static final String AddClient = "INSERT INTO user (Username, Password, Name, Contact, Verified) VALUES (?, ?, ?, ?, ?)";
 		public static final String AddAdmin = "INSERT INTO admin (Username, Password, Name, Contact, Verified) VALUES (?, ?, ?, ?, ?)";
 		public static final String VerifyClient = "UPDATE user SET Verified='Y' WHERE Username = ?";
