2918ccdaad0c7ccd6917a5a751553637ab8ffcd2
da201112014 <da201112014@gmail.com>
2012-10-28 19:33:40 +0000

    Non adaptive test

---
+++ cadet/src/org/cadet/client/controller/SubmitAnswerNA.java
@@ -0,0 +1,67 @@
+package org.cadet.client.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.cadet.client.model.NonAdaptiveTest;
+import org.json.JSONException;
+
+/**
+ * Servlet implementation class SubmitAnswerNA
+ */
+@WebServlet("/Test/NonAdaptive/SubmitAnswer")
+public class SubmitAnswerNA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SubmitAnswerNA() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendError(403);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession session = request.getSession();
+		NonAdaptiveTest test;
+		String Category;
+		String QNO;
+		String QID;
+		String ANS;
+		try{
+			test = (NonAdaptiveTest) session.getAttribute("test");
+			Category = request.getParameter("category").toString();
+			QNO = request.getParameter("QNO").toString();
+			QID = request.getParameter("QID").toString();
+			ANS = request.getParameter("ans").toString();
+		}catch(Exception e){
+			response.sendError(403);
+			return;
+		}
+		
+		try {
+			test.Store_Answer(Category, QNO, QID, ANS);
+		} catch (JSONException e) {
+			response.sendError(500);
+			return;
+		}
+		
+	}
+
+}
---
+++ cadet/src/org/cadet/client/controller/SubmitTestNA.java
@@ -0,0 +1,63 @@
+package org.cadet.client.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.cadet.client.model.NonAdaptiveTest;
+import org.cadet.util.model.DatabaseConnection;
+
+/**
+ * Servlet implementation class SubmitAnswerNA
+ */
+@WebServlet("/Test/NonAdaptive/SubmitTest")
+public class SubmitTestNA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SubmitTestNA() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendError(403);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession session = request.getSession();
+		NonAdaptiveTest test;
+		String username;
+		Connection connection = DatabaseConnection.getInstance().getDbConnection();
+		try{
+			test = (NonAdaptiveTest) session.getAttribute("test");
+			username = session.getAttribute("user").toString();
+		}catch(Exception e){
+			response.sendError(403);
+			return;
+		}
+		
+			test.process_Answers(connection, username);
+			
+		session.setAttribute("test", null);
+		session.setAttribute("testCategory", null);
+		
+			response.sendRedirect("TestComplete");
+		
+	}
+
+}
