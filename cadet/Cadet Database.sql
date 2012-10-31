dcabded4bcf94845e9566adfb38ce0cd16f45f2d
aadesh33 <aadesh33@gmail.com>
2012-10-31 19:59:55 +0000

    Database-6.0

--- cadet/Cadet Database.sql
+++ cadet/Cadet Database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2012 at 07:11 AM
--- Server version: 5.5.20
--- PHP Version: 5.3.10
+-- Generation Time: Oct 30, 2012 at 12:36 PM
+-- Server version: 5.1.53-community-log
+-- PHP Version: 5.3.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,13 +27,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admin` (
-  `Username` varchar(25) NOT NULL COMMENT 'Stores Username Of Admin',
+  `AUserName` varchar(25) NOT NULL COMMENT 'Stores Username Of Admin',
   `Password` varchar(250) NOT NULL COMMENT 'Stores The Password of Admin',
   `Name` varchar(30) NOT NULL COMMENT 'Stores Name of Admin',
   `Contact` int(15) NOT NULL COMMENT 'Stores Contact of Admin',
-  `Verified` varchar(1) NOT NULL,
-  PRIMARY KEY (`Username`)
+  `Verified` varchar(1) NOT NULL COMMENT 'Stores that User is verified or not',
+  PRIMARY KEY (`AUserName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Admin Table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate`
+--
+
+CREATE TABLE IF NOT EXISTS `candidate` (
+  `CUserName` varchar(25) NOT NULL COMMENT 'Stores Username of User',
+  `Password` varchar(250) NOT NULL COMMENT 'Stores Password of User',
+  `Name` varchar(30) NOT NULL COMMENT 'Stores Name of User',
+  `Contact` int(15) NOT NULL COMMENT 'Stores Contact of User',
+  `CandidateCategoryName` varchar(25) NOT NULL COMMENT 'Stores User_Category_Name',
+  `Verified` varchar(1) NOT NULL COMMENT 'Stores whether user is Verified or not',
+  PRIMARY KEY (`CUserName`),
+  KEY `UserCategoryName` (`CandidateCategoryName`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores Details of User';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidatecategory`
+--
+
+CREATE TABLE IF NOT EXISTS `candidatecategory` (
+  `CandidateCategoryName` varchar(25) NOT NULL COMMENT 'Stores User Category Name',
+  PRIMARY KEY (`CandidateCategoryName`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores User Category Details';
 
 -- --------------------------------------------------------
 
@@ -42,46 +70,47 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `Categoryname` varchar(15) NOT NULL COMMENT 'Stores Name of Category',
+  `CategoryId` int(3) NOT NULL COMMENT 'Stores the Category ID',
+  `CategoryName` varchar(15) NOT NULL COMMENT 'Stores Name of Category',
   `Description` varchar(50) NOT NULL COMMENT 'Stores Description of Category',
   `Ausername` varchar(25) NOT NULL COMMENT 'Stores the name of admin who added the Category',
-  PRIMARY KEY (`Categoryname`),
+  PRIMARY KEY (`CategoryId`),
   KEY `Ausername` (`Ausername`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores Details of Category';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `level_marks`
+-- Table structure for table `levelmarks`
 --
 
-CREATE TABLE IF NOT EXISTS `level_marks` (
-  `Lid` int(5) NOT NULL COMMENT 'Stores the level of the question',
-  `Testid` int(5) NOT NULL COMMENT 'Stores ID of the Test',
+CREATE TABLE IF NOT EXISTS `levelmarks` (
+  `LevelId` int(5) NOT NULL COMMENT 'Stores the level of the question',
+  `TestId` int(5) NOT NULL COMMENT 'Stores ID of the Test',
   `Marks` int(5) NOT NULL COMMENT 'Stores the marks of a particular level',
-  PRIMARY KEY (`Lid`,`Testid`),
-  KEY `Testid` (`Testid`)
+  PRIMARY KEY (`LevelId`,`TestId`),
+  KEY `Testid` (`TestId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores what level has how many marks';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `question_bank`
+-- Table structure for table `questionbank`
 --
 
-CREATE TABLE IF NOT EXISTS `question_bank` (
-  `QID` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Stores Question''s ID',
-  `CName` varchar(15) NOT NULL COMMENT 'Stores Category of the Question',
+CREATE TABLE IF NOT EXISTS `questionbank` (
+  `QuestionId` int(6) NOT NULL AUTO_INCREMENT COMMENT 'Stores Question''s ID',
+  `CategoryId` int(3) NOT NULL COMMENT 'Stores the Id of Category',
+  `LevelId` int(5) NOT NULL COMMENT 'Stores the Level Id',
   `Question` text NOT NULL COMMENT 'Stores Question',
-  `Option A` text NOT NULL COMMENT 'Stores Option A of the Question',
-  `Option B` text NOT NULL COMMENT 'Stores Option B of the Question',
-  `Option C` text NOT NULL COMMENT 'Stores Option C of the Question',
-  `Option D` text NOT NULL COMMENT 'Stores Option D of the Question',
-  `Correct Option` text NOT NULL COMMENT 'Stores Correct Option of the Question',
-  `Lid` int(5) NOT NULL COMMENT 'Stores Level of the Question',
-  PRIMARY KEY (`QID`),
-  KEY `CName` (`CName`),
-  KEY `Lid` (`Lid`)
+  `OptA` text NOT NULL COMMENT 'Stores Option A of the Question',
+  `OptB` text NOT NULL COMMENT 'Stores Option B of the Question',
+  `OptC` text NOT NULL COMMENT 'Stores Option C of the Question',
+  `OptD` text NOT NULL COMMENT 'Stores Option D of the Question',
+  `CorrectAnswer` text NOT NULL COMMENT 'Stores Correct Option of the Question',
+  PRIMARY KEY (`QuestionId`),
+  KEY `CategoryId` (`CategoryId`,`LevelId`),
+  KEY `LevelId` (`LevelId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores the details of the all the Question' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -91,14 +120,14 @@ CREATE TABLE IF NOT EXISTS `question_bank` (
 --
 
 CREATE TABLE IF NOT EXISTS `result` (
-  `SUsername` varchar(25) NOT NULL COMMENT 'Stores the Name of the user who appeared for the Test',
-  `Testid` int(5) NOT NULL COMMENT 'Stores the Test ID for which the user appeared for the Test',
+  `CUserName` varchar(25) NOT NULL COMMENT 'Stores the Name of the user who appeared for the Test',
+  `TestId` int(5) NOT NULL COMMENT 'Stores the Test ID for which the user appeared for the Test',
   `Marks` int(5) NOT NULL COMMENT 'Stores Marks of the User',
   `Attempted` int(5) NOT NULL COMMENT 'Stores the number of attempted question',
   `Correct` int(5) NOT NULL COMMENT 'Stores number of correct question',
-  PRIMARY KEY (`SUsername`,`Testid`),
-  KEY `Testid` (`Testid`),
-  KEY `SUsername` (`SUsername`)
+  PRIMARY KEY (`CUserName`,`TestId`),
+  KEY `Testid` (`TestId`),
+  KEY `SUsername` (`CUserName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores the Result of the User';
 
 -- --------------------------------------------------------
@@ -108,126 +137,98 @@ CREATE TABLE IF NOT EXISTS `result` (
 --
 
 CREATE TABLE IF NOT EXISTS `test` (
-  `Testid` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Stores the test ID',
-  `Type` varchar(15) NOT NULL COMMENT 'Stores the type of the Test',
-  `Testname` varchar(30) NOT NULL,
-  `Tdate` date NOT NULL COMMENT 'Stores the date of the test',
+  `TestId` int(5) NOT NULL AUTO_INCREMENT COMMENT 'Stores the test ID',
+  `TestType` varchar(15) NOT NULL COMMENT 'Stores the type of the Test',
+  `TestName` varchar(30) NOT NULL COMMENT 'Stores the Name of the Test',
+  `TestDesc` varchar(255) NOT NULL COMMENT 'Stores the Description of the test',
+  `TestDate` date NOT NULL COMMENT 'Stores the date of the test',
   `StartTime` datetime NOT NULL COMMENT 'Stores start time of the test',
   `EndTime` datetime NOT NULL COMMENT 'Stores end time of the test',
-  `Tduration` int(5) NOT NULL COMMENT 'Stores the duration of the test',
-  `Initialdifficulty` int(5) NOT NULL COMMENT 'Stores the initial difficulty of the Test',
-  `Testdesc` varchar(250) NOT NULL COMMENT 'Stores Description of the test',
-  PRIMARY KEY (`Testid`)
+  `TestDuration` int(5) NOT NULL COMMENT 'Stores the duration of the test',
+  `InitialDifficulty` int(5) NOT NULL COMMENT 'Stores the initial difficulty of the Test',
+  PRIMARY KEY (`TestId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores the Details of the Test' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_category`
+-- Table structure for table `testcategory`
 --
 
-CREATE TABLE IF NOT EXISTS `test_category` (
-  `TestID` int(5) NOT NULL,
-  `Categoryname` varchar(15) NOT NULL,
-  `Per_Category_Time` int(3) NOT NULL,
-  `Question_Per_Category` int(3) NOT NULL,
-  PRIMARY KEY (`TestID`,`Categoryname`),
-  KEY `Categoryname` (`Categoryname`),
-  KEY `TestID` (`TestID`)
+CREATE TABLE IF NOT EXISTS `testcategory` (
+  `TestId` int(5) NOT NULL,
+  `CategoryId` int(5) NOT NULL,
+  `TimePerCategory` int(3) NOT NULL COMMENT 'Stores the time per category in MINUTES',
+  `QuestionPerCategory` int(3) NOT NULL COMMENT 'Stores the number of Question per Category',
+  PRIMARY KEY (`TestId`,`CategoryId`),
+  KEY `Categoryname` (`CategoryId`),
+  KEY `TestID` (`TestId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_question`
+-- Table structure for table `testquestion`
 --
 
-CREATE TABLE IF NOT EXISTS `test_question` (
-  `Testid` int(5) NOT NULL COMMENT 'Stores the testid',
-  `Quesitonid` int(5) NOT NULL COMMENT 'Stores question id',
-  `Negativemark` int(5) NOT NULL COMMENT 'Stores Negative Mark For the Questions in  Test',
-  PRIMARY KEY (`Testid`,`Quesitonid`),
-  KEY `Quesitonid` (`Quesitonid`),
-  KEY `Testid` (`Testid`)
+CREATE TABLE IF NOT EXISTS `testquestion` (
+  `TestId` int(5) NOT NULL COMMENT 'Stores the testid',
+  `QuesitonId` int(5) NOT NULL COMMENT 'Stores question id',
+  `NegMark` int(5) NOT NULL COMMENT 'Stores Negative Mark For the Questions in  Test',
+  PRIMARY KEY (`TestId`,`QuesitonId`),
+  KEY `Quesitonid` (`QuesitonId`),
+  KEY `Testid` (`TestId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores the list of question (used only if non adaptive)';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `Username` varchar(25) NOT NULL COMMENT 'Stores Username of User',
-  `Password` varchar(250) NOT NULL COMMENT 'Stores Password of User',
-  `Name` varchar(30) NOT NULL COMMENT 'Stores Name of User',
-  `Contact` int(15) NOT NULL COMMENT 'Stores Contact of User',
-  `UserCategoryName` varchar(25) NOT NULL COMMENT 'Stores User_Category_Name',
-  `Verified` varchar(1) NOT NULL,
-  PRIMARY KEY (`Username`),
-  KEY `UserCategoryName` (`UserCategoryName`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores Details of User';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_category`
---
-
-CREATE TABLE IF NOT EXISTS `user_category` (
-  `UserCategoryName` varchar(25) NOT NULL COMMENT 'Stores User Category Name',
-  PRIMARY KEY (`UserCategoryName`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores User Category Details';
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD CONSTRAINT `candidate_ibfk_1` FOREIGN KEY (`CandidateCategoryName`) REFERENCES `candidatecategory` (`CandidateCategoryName`);
+
+--
 -- Constraints for table `category`
 --
 ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`Ausername`) REFERENCES `admin` (`Username`);
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`Ausername`) REFERENCES `admin` (`AUserName`);
 
 --
--- Constraints for table `level_marks`
+-- Constraints for table `levelmarks`
 --
-ALTER TABLE `level_marks`
-  ADD CONSTRAINT `level_marks_ibfk_1` FOREIGN KEY (`Testid`) REFERENCES `test` (`Testid`);
+ALTER TABLE `levelmarks`
+  ADD CONSTRAINT `levelmarks_ibfk_1` FOREIGN KEY (`TestId`) REFERENCES `test` (`TestId`);
 
 --
--- Constraints for table `question_bank`
+-- Constraints for table `questionbank`
 --
-ALTER TABLE `question_bank`
-  ADD CONSTRAINT `question_bank_ibfk_1` FOREIGN KEY (`CName`) REFERENCES `category` (`Categoryname`),
-  ADD CONSTRAINT `question_bank_ibfk_2` FOREIGN KEY (`Lid`) REFERENCES `level_marks` (`Lid`);
+ALTER TABLE `questionbank`
+  ADD CONSTRAINT `questionbank_ibfk_2` FOREIGN KEY (`LevelId`) REFERENCES `levelmarks` (`LevelId`),
+  ADD CONSTRAINT `questionbank_ibfk_1` FOREIGN KEY (`CategoryId`) REFERENCES `category` (`CategoryId`);
 
 --
 -- Constraints for table `result`
 --
 ALTER TABLE `result`
-  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`SUsername`) REFERENCES `user` (`Username`),
-  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`Testid`) REFERENCES `test` (`Testid`);
+  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`TestId`) REFERENCES `test` (`TestId`),
+  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`CUserName`) REFERENCES `candidate` (`CUserName`);
 
 --
--- Constraints for table `test_category`
+-- Constraints for table `testcategory`
 --
-ALTER TABLE `test_category`
-  ADD CONSTRAINT `test_category_ibfk_1` FOREIGN KEY (`TestID`) REFERENCES `test` (`Testid`),
-  ADD CONSTRAINT `test_category_ibfk_2` FOREIGN KEY (`Categoryname`) REFERENCES `category` (`Categoryname`);
+ALTER TABLE `testcategory`
+  ADD CONSTRAINT `testcategory_ibfk_2` FOREIGN KEY (`CategoryId`) REFERENCES `category` (`CategoryId`),
+  ADD CONSTRAINT `testcategory_ibfk_1` FOREIGN KEY (`TestId`) REFERENCES `test` (`TestId`);
 
 --
--- Constraints for table `test_question`
+-- Constraints for table `testquestion`
 --
-ALTER TABLE `test_question`
-  ADD CONSTRAINT `test_question_ibfk_1` FOREIGN KEY (`Testid`) REFERENCES `test` (`Testid`),
-  ADD CONSTRAINT `test_question_ibfk_2` FOREIGN KEY (`Quesitonid`) REFERENCES `question_bank` (`QID`);
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`UserCategoryName`) REFERENCES `user_category` (`UserCategoryName`);
+ALTER TABLE `testquestion`
+  ADD CONSTRAINT `testquestion_ibfk_2` FOREIGN KEY (`QuesitonId`) REFERENCES `questionbank` (`QuestionId`),
+  ADD CONSTRAINT `testquestion_ibfk_1` FOREIGN KEY (`TestId`) REFERENCES `test` (`TestId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
