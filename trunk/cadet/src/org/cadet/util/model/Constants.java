c67eb0c8299ca1aec83de2c0421b9e52cc6d2989
da201112014@gmail.com <da201112014@gmail.com@798e3413-0ab0-605c-d195-c559410ce5df>
2012-11-02 11:53:02 +0000

    Constants change

--- trunk/cadet/src/org/cadet/util/model/Constants.java
+++ trunk/cadet/src/org/cadet/util/model/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
 		public static final String retriveTests = "SELECT * FROM test";
 		public static final String retriveLastInsertID = "SELECT last_insert_id()";
 		public static final String isClientAvailable = "SELECT COUNT(DISTINCT CUserName) AS count FROM candidate WHERE CUserName = ";
-		public static final String AddClient = "INSERT INTO candidate (CUserName, Password, Name, Contact, Category, Verified) VALUES (?, ?, ?, ?, ?, ?)";
+		public static final String AddClient = "INSERT INTO candidate (CUserName, Password, Name, Contact, CandidateCategoryName, Verified) VALUES (?, ?, ?, ?, ?, ?)";
 		public static final String AddAdmin = "INSERT INTO admin (Username, Password, Name, Contact, Verified) VALUES (?, ?, ?, ?, ?)";
 		public static final String AddTest = "INSERT INTO test (TestName,TestType,TestDesc) VALUES (?,?,?)";
 		public static final String VerifyClient = "UPDATE candidate SET Verified='Y' WHERE CUserName = ?";
@@ -45,8 +45,8 @@ public class Constants {
 	
 	public static final class email{
 		public static final String SMTP_HOST_NAME = "smtp.gmail.com";
-		public static final String EmailUsername = "da201112014@gmail.com";
-		public static final String EmailPassword = "JaY#1010";
+		public static final String EmailUsername = "cadet.daiict@gmail.com";
+		public static final String EmailPassword = "cadet.daiict";
 		public static final Integer SMTP_PORT = 465;
 		public static final String SMTP_SSL = "true";
 		public static final String SMTP_AUTH = "true";
