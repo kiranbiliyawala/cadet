7a8080592adda878d723ce7e3948e2cdf5a52e35
ucant2seeme@gmail.com <ucant2seeme@gmail.com@798e3413-0ab0-605c-d195-c559410ce5df>
2012-11-03 11:35:28 +0000

    Dashboard controller added

---
+++ trunk/cadet/src/org/cadet/client/controller/DashboardServlet.java
@@ -0,0 +1,68 @@
+package org.cadet.client.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.cadet.util.model.DatabaseConnection;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.cadet.client.model.DashboardModel;;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/client/DashboardServlet")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		JSONObject data = null;
+
+
+	    DatabaseConnection dbConn = DatabaseConnection.getInstance();
+	    Connection dbConnection = dbConn.getDbConnection();
+	    try {
+			data = DashboardModel.getTests(dbConnection);
+			data.put("result", true);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			System.out.println(data);
+			out.println(data);
+		} catch (SQLException | JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
