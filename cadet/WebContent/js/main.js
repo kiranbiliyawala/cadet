564395fdefc4162dfaa4b48e7a7e6c8cbb98fd2e
ucant2seeme <ucant2seeme@gmail.com>
2012-11-03 11:01:39 +0000

    Javascript and Login details

--- cadet/WebContent/js/main.js
+++ cadet/WebContent/js/main.js
@@ -1,3 +1,17 @@
 function transferRegisteration(){
 	window.location="./pages/Register.jsp";
 }
+
+function submit_val(){
+	
+	var pass = document.getElementById("password");
+	
+	pass.value = hashmap(pass.value);
+
+}
+
+function hashmap(input){
+	var shaobj = new jsSHA(input,"ASCII");
+	var hash = shaobj.getHash("SHA-512", "HEX");
+	return hash;
+}
