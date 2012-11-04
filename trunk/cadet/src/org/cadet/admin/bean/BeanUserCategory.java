2c689d840a0401b968b0f3b961372bd2977368c3
aadesh33@gmail.com <aadesh33@gmail.com@798e3413-0ab0-605c-d195-c559410ce5df>
2012-11-04 17:51:40 +0000

    BeanUserCategory

---
+++ trunk/cadet/src/org/cadet/admin/bean/BeanUserCategory.java
@@ -0,0 +1,14 @@
+package org.cadet.admin.bean;
+
+public class BeanUserCategory {
+	String CandidateCategory;
+
+	public String getCandidateCategory() {
+		return CandidateCategory;
+	}
+
+	public void setCandidateCategory(String candidateCategory) {
+		CandidateCategory = candidateCategory;
+	}
+	
+}
