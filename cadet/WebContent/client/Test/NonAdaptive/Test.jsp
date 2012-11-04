6b89993bab45469515b91b64beb48638eb4d65e0
da201112014 <da201112014@gmail.com>
2012-11-04 12:09:10 +0000

    NonAdaptive solved

--- cadet/WebContent/client/Test/NonAdaptive/Test.jsp
+++ cadet/WebContent/client/Test/NonAdaptive/Test.jsp
@@ -3,33 +3,46 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <script src="../../../js/jquery-1.8.2.js"></script>
+<script src="../../../js/bootstrap.js"></script>
 <script src="js/NATEST.js"></script>
-<link rel="stylesheet" type="text/css" href="css na.css">
+<link rel="stylesheet" type="text/css" href="../../../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/NATEST.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><%= request.getAttribute("TestTime") %></title>
+
+<script type="text/javascript">
+
+var questiondistribution = <%= request.getAttribute("CatDistribution")%>;
+
+</script>
+
 </head>
-<body class="body">
-	<div id = "catselector" class="question_portion">
+<body class="body" onload="body_load()">
+	<div id = "catselector" class="accordion question_portion">
+	<div style="margin-top: 5%"></div>
+	<div class="Category" style="font-size: 20px;margin-bottom: .5%"> Categories </div>
 	<%= request.getAttribute("Catselector") %>
 	</div>
 	<div class="question_portion" id = "question_portion">
 		<div class="Question_no">
         <h1 class="QNO" id="QNO">1</h1>
          <br/>
-         <div class="flag_check">
+         <form id="flag_form" class="flag_check">
             <input type="checkbox" id="flag"/>
 			Mark <br />Question<br /> as flaged
-         </div>
+         </form>
          </div>
 		<div class="Question" id="Question">
 			Question
 				A, B, C and D go for a picnic. When A stands on a weighing machine, B also climbs on, and the weight shown was 132 kg. When B 	stands, C also climbs on, and the machine shows 130 kg. Similarly the weight of C and D is found as 102 kg and that of B and D is 116 kg. What is D's weight??
 		</div>
-		<form id="Options" class="Options" action="">
-			<input type="radio" name="Answer" id="option1" value="A"/>Option1<br />
-			<input type="radio" name="Answer" id="option2" value="B"/>Option2<br />
-			<input type="radio" name="Answer" id="option3" value="C"/>Option3<br />
-            <input type="radio" name="Answer" id="option4" value="D"/>Option4<br />
+		<form id="Options" method="post" class="Options" action="SubmitTest">
+			<input type="radio" name="Answer" id="option1" value="A"/><span id="optn1">Option1</span><br />
+			<input type="radio" name="Answer" id="option2" value="B"/><span id="optn2">Option2</span><br />
+			<input type="radio" name="Answer" id="option3" value="C"/><span id="optn3">Option3</span><br />
+            <input type="radio" name="Answer" id="option4" value="D"/><span id="optn4">Option4</span><br />
+            <br/>
+                    <a href="javascript:void(0)" onclick="clear_answer()">Clear Answer</a>
          </form>
 		</div>
 </div>
@@ -56,13 +69,12 @@
     </div>
     </div>
       
-	
 	<div class="Controls" id = "Controls">
-		<button class="btn prev_next_btn" type="button" name="Previous" text="Previous" style="float:left;"/>&lt; Previous</button>
-        <button class="btn section_btn" type="button" name="Change Section" text="Change Section"/>Change Section</button>
-		<button class="btn test_fin_btn" type="button" onclick="" name="Finish Test" text="Finish Test"/>Finish Test</button>
-        <button class="btn prev_next_btn" type="button" name="Next&ampSubmit" text="Next &amp Submit" style="float:right"/>Next &amp Submit &gt;</button>
-        <button class="btn skip_btn" type="button" onclick="clear_answer()" name="Skip" text="Skip" style="float:right"/>Skip</button>
+		<button class="butn prev_next_btn" type="button" onclick="previous()" name="Previous" text="Previous" style="float:left;"/>&lt; Previous</button>
+        <button class="butn section_btn" type="button" onclick="show_catselector()" name="Change Section" text="Change Section"/>Change Section</button>
+		<button class="butn test_fin_btn" type="button" onclick="finish_test_confirm()" name="Finish Test" text="Finish Test"/>Finish Test</button>
+        <button class="butn prev_next_btn" type="button" onclick="submit_next()" name="Next&ampSubmit" text="Next &amp Submit" style="float:right"/>Next &amp Submit &gt;</button>
+        <button class="butn skip_btn" type="button" onclick="skip()" name="Skip" text="Skip" style="float:right"/>Skip</button>
 	</div>
 </body>
 </html>
--- cadet/WebContent/client/Test/NonAdaptive/js/NATEST.js
+++ cadet/WebContent/client/Test/NonAdaptive/js/NATEST.js
@@ -14,21 +14,22 @@ var lock = false;
 
 function update_Attempted(command,cat,question){
 	
+	var internal = "#Section #Qstub"+cat+question+", #catselector #Qstub"+cat+question;
 
 	if(command=="Attempted"){
-		$("#Qstub"+cat+question).addClass("Attempted");
-		$("#Qstub"+cat+question).removeClass("NotAttempted");
-		$("#Qstub"+cat+question).removeClass("Flagged");
+		$(internal).addClass("Attempted");
+		$(internal).removeClass("NotAttempted");
+		$(internal).removeClass("Flagged");
 	}
 	else if(command=="Flagged"){
-		$("#Qstub"+cat+question).addClass("Flagged");
-		$("#Qstub"+cat+question).removeClass("NotAttempted");
-		$("#Qstub"+cat+question).removeClass("Attempted");
+		$(internal).addClass("Flagged");
+		$(internal).removeClass("NotAttempted");
+		$(internal).removeClass("Attempted");
 	}
 	else if(command=="NotAttempted"){
-		$("#Qstub"+cat+question).addClass("NotAttempted");
-		$("#Qstub"+cat+question).removeClass("Attempted");
-		$("#Qstub"+cat+question).removeClass("Flagged");
+		$(internal).addClass("NotAttempted");
+		$(internal).removeClass("Attempted");
+		$(internal).removeClass("Flagged");
 	}
 }
 
@@ -39,78 +40,101 @@ function updatecatview(cat){
 	$("#dispcat"+cat).show(2);
 }
 
-function update_data(){
-	$("#QNO").html(qno);
-	$("#Question").html(Question);
-}
-
 function ajax_caller(callurl,data_value,async_value,callback){
 	$.ajax({
-		url:callurl,
-		data:data_value,
-		async:async_value,
-		succsess:callback(result)
+		url: callurl,
+		data: data_value,
+		async: async_value,
+		type:'POST',
+		success: function(data) {
+            callback(data);
+        }
 	}
 		);
 }
 
 
 function show_cat(covercat){
-	$(".CatCover").hide();
-	$("#"+covercat).show();
+	$(".CatCover:not(#"+covercat+")").hide();
 }
 
 function change_question_data(){
-	$("#option1").html(optiona);
-	$("#option2").html(optionb);
-	$("#option3").html(optionc);
-	$("#option4").html(optiond);
+	$("#optn1").html(optiona);
+	$("#optn2").html(optionb);
+	$("#optn3").html(optionc);
+	$("#optn4").html(optiond);
 	$("#Question").html(Question);
 	$("QNO").html(qno);
 	
-	if(answer==A){
+	if(answer=='A'){
 	$("#option1").attr("checked",true);	
 	}
-	if(answer==B){
+	if(answer=='B'){
 	$("#option2").attr("checked",true);	
 	}
-	if(answer==C){
+	if(answer=='C'){
 	$("#option3").attr("checked",true);	
 	}
-	if(answer==D){
+	if(answer=='D'){
 	$("#option4").attr("checked",true);	
 	}
 }
 
-function any(){}
 
 function submit_next(){
-
+	if($("#flag").is(":checked")){
+			remove_answer();
+			set_flag();
+			skip();
+			return;
+		}
+		
 	var ans = $("input[name=Answer]:checked","#Options").val();
 	if(ans!=null)
 	{
-		submit_answer(ans);
-	}else if(attempted=="Attempted")
+	submit_answer(ans);
+	}else if($("#Qstub"+category+qno).hasClass('Attempted'))
 	{
 		remove_answer();
 	}
-	
-	if(flag=true){
-		remove_answer();
-	}
 		
-	var maxamount = questiondistribution.category.NoOfQuestions;
-	if(qno<maxamount){
-		qno = qno+1;
-	}else{
-		show_catselector();
-	}
+	skip();
 }
 
+function any(result){
+}
+
+function remove_answer(){
+	var dat = {
+			'category': category,
+			'QNO': qno,
+			'QID': QID
+	};
+	ajax_caller("RemoveAnswer",dat,false,any);
+	update_Attempted("NotAttempted",category,qno);
+}
+
+function submit_answer(ans){
+	var dat = {
+			'CATEGORY': category,
+			'QNO': qno,
+			'QID': QID,
+			'ANS':ans
+	};
+	ajax_caller("SubmitAnswer",dat,false,any);
+	update_Attempted("Attempted",category,qno);
+}
+
+function set_flag() {
+	update_Attempted("Flagged",category,qno);
+}
+
+
 function skip(){
-	var maxamount = questiondistribution.category.NoOfQuestions;
+	var maxamount = questiondistribution[category];
 	if(qno<maxamount){
 		qno = qno+1;
+		change_que(category, qno);
 	}else{
 		show_catselector();
 	}
@@ -119,12 +143,14 @@ function skip(){
 function previous(){
 	if(qno>1){
 		qno = qno-1;
+		change_que(category, qno);
 	}else{
 		show_catselector();
 	}
 }
 
 function show_catselector(){
+	$(".CatCover").show();
 	$("#question_portion").hide();
 	$("#Section").hide();
 	$("#Controls").hide();
@@ -147,21 +173,27 @@ function clear_answer(){
 }
 
 function change_que(cat,queno){
-refresh_que(cat,queno)
-
+	show_cat("cover"+cat);
+	hide_catselector();
+	clear_answer();
+refresh_que(cat,queno);
 }
 
 function refresh_que(cat,queno)
 {
-var data={CAT:cat,QNO:queno};
+var data={
+	'CAT':cat,
+	'QNO':queno
+	};
 
 category=cat;
 qno=queno;
-ajax_caller("",data,false,change_question_details);
-updatecatview(cat);
+ajax_caller("GetQuestion",data,false,change_question_details);
 }
 
-function change_question_details(result){
+function change_question_details(res){
+	
+	var result = JSON.parse(res);
 	
 Question=result.QUESTION;
 optiona=result.OPTIONA;
@@ -173,17 +205,21 @@ QID=result.QID;
 change_question_data();
 }
 
+
+function body_load(){
+	show_catselector();
+}
+
 function finish_test_confirm(){
-var x;
 var r=confirm("Are you sure to finish the test ?");
 if (r==true)
   {
   finish_test();
 }
-document.getElementById("demo").innerHTML=x;
 
 }
 function finish_test()
 {
-
+	$("form").submit();
+		alert("a");
 }
