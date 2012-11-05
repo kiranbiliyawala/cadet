0a9f3af07fa7000cd11d06349ef1d697dd9ba5d9
ucant2seeme <ucant2seeme@gmail.com>
2012-11-05 12:35:53 +0000

    registerForTest added

--- cadet/WebContent/client/js/main.js
+++ cadet/WebContent/client/js/main.js
@@ -7,6 +7,10 @@ function submit_val(){
 
 }
 
+function start_test(testtype,testid){
+	window.open('Test/'+testtype+'/CreateTest?testid='+testid,'_blank','location=0,menubar=0,statusbar=0,titelebar=0,toolbar=0',false);
+}
+
 function hashmap(input){
 	var shaobj = new jsSHA(input,"ASCII");
 	var hash = shaobj.getHash("SHA-512", "HEX");
