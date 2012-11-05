65544c1b85c113e88805004da26942e08b5eb498
ucant2seeme <ucant2seeme@gmail.com>
2012-11-05 12:32:55 +0000

    NonAdaptiveTest and DashboardModel change

--- cadet/src/org/cadet/client/model/DashboardModel.java
+++ cadet/src/org/cadet/client/model/DashboardModel.java
@@ -1,6 +1,7 @@
 package org.cadet.client.model;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -25,22 +26,35 @@ public class DashboardModel {
 		PreparedStatement statement = connection.prepareStatement(Constants.sqlCommands.getDashboardTests);
 		statement.setString(1, username);
 		ResultSet rs = statement.executeQuery();
-		while (rs.next()) {
-
-		    temp = new DashboardBean();
-		    temp.setTestId(rs.getInt("TestId"));
-		    temp.setTestName(rs.getString("TestName"));
-		    temp.setTestDate(rs.getDate("TestDate"));
-		    temp.setTestDuration(rs.getInt("TestDuration"));
-
-		    result.add(temp);
-		}
+//		while (rs.next()) {
+//
+//		    
+////		    temp.setTestId(rs.getInt("TestId"));
+////		    temp.setTestName(rs.getString("TestName"));
+////		    temp.setTestDate(rs.getDate("TestDate"));
+////		    temp.setTestDuration(rs.getInt("TestDuration"));
+//		    
+//		}
+		temp = new DashboardBean();
+		temp.setTestId(1);
+		temp.setTestType("NonAdaptive");
+	    temp.setTestName("DAIICT");
+	    temp.setTestDate(new Date(100));
+	    temp.setTestDuration(120);
+	    result.add(temp);
+	    temp = new DashboardBean();
+	    temp.setTestId(2);
+	    temp.setTestType("NonAdaptive");
+	    temp.setTestName("DAIICT");
+	    temp.setTestDate(new Date(100));
+	    temp.setTestDuration(120);
+	    result.add(temp);
 		rs.close();
 		statement.close();
 
 		JSONObject data = new JSONObject();
 		data.put("testList", result.toArray(new DashboardBean[result.size()]));
-
+		System.out.println("dashboard: " + data);
 		return data;
 	    }
 	
