4bbd3cd37088f888601da8ebd81478284cfe7ce6
ucant2seeme <ucant2seeme@gmail.com>
2012-11-05 10:48:35 +0000

    Rajan Commit

---
+++ cadet/src/org/cadet/client/controller/GetCandidateProfileServlet.java
@@ -0,0 +1,73 @@
+package org.cadet.client.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.cadet.client.model.CandidateProfileModel;
+import org.cadet.util.model.DatabaseConnection;
+
+/**
+ * Servlet implementation class GetCandidateProfileServlet
+ */
+@WebServlet("/client/profile/GetCandidateProfileServlet")
+public class GetCandidateProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetCandidateProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		DatabaseConnection dbConn = DatabaseConnection.getInstance();
+	    Connection dbConnection = dbConn.getDbConnection();
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        System.out.println("in servlet");
+        HttpSession cadetsession = request.getSession();
+		String CUserName = (String) cadetsession.getAttribute("user"); // "cadet.daiict@gmail.com"; 
+        
+        CandidateProfileModel cpm = new CandidateProfileModel(dbConnection); 
+        try {            
+
+            List<String> list_CandidateProfileData= cpm.getCandidateProfileData(CUserName);
+            System.out.println("From servlet:" + list_CandidateProfileData);
+            request.setAttribute("CandidateProfileData", list_CandidateProfileData);
+            List<String> list_CandidateCategory=cpm.getCandidateCategory();
+            System.out.println("From servlet:" + list_CandidateCategory);
+            request.setAttribute("CandidateCategory", list_CandidateCategory);
+
+
+           request.getRequestDispatcher("changedetails.jsp").forward(request,response);
+           
+           
+        } finally { 
+            out.close();
+        }
+		
+	}
+
+}
