89837369301fc3422742cebdad588640851c2f89
ucant2seeme@gmail.com <ucant2seeme@gmail.com@798e3413-0ab0-605c-d195-c559410ce5df>
2012-11-05 23:30:30 +0000

    Client javascript

--- trunk/cadet/WebContent/client/js/main.js
+++ trunk/cadet/WebContent/client/js/main.js
@@ -1,4 +1,8 @@
 
+function start_test(testtype,testid){
+	window.open('Test/'+testtype+'/CreateTest?testid='+testid,'_blank','location=0,menubar=0,statusbar=0,titelebar=0,toolbar=0',false);
+}
+
 function submit_val(){
 	
 	var pass = document.getElementById("password");
@@ -7,34 +11,8 @@ function submit_val(){
 
 }
 
-
-$(document).ready(
-		function(){
-			$.post("DashboardServlet",
-					{ requestType : "getTests" },
-					function(data,textStatus,xhr) {
-
-						try {
-							if(data.result===true) {
-								var src = $("#gettests").html();
-								var template = Handlebars.compile(src);
-								var output = template(data);
-								$("#tblTests tbody").append(output);
-							}
-						} catch(e) { bootbox.alert(e.status+"\n"+e.message); }
-			});
-		}
-);
-
-
-
-function start_test(testtype,testid){
-	window.open('Test/'+testtype+'/CreateTest?testid='+testid,'_blank','location=0,menubar=0,statusbar=0,titelebar=0,toolbar=0',false);
-}
-
 function hashmap(input){
 	var shaobj = new jsSHA(input,"ASCII");
 	var hash = shaobj.getHash("SHA-512", "HEX");
 	return hash;
 }
-
