20fe2b3c3606b3116003f688512e5e428550a1d0
varunjamdar <varunjamdar@798e3413-0ab0-605c-d195-c559410ce5df>
2012-11-06 01:40:48 +0000

    Model Layer finished !! Phew..

---
+++ cadet/src/org/cadet/client/bean/Question.java
@@ -0,0 +1,81 @@
+package org.cadet.client.model.adaptive;
+
+
+/**
+ * @author Varun Jamdar
+ *
+ */
+public class Question {
+	
+	private int questionId;
+	private int level;
+	private int marks;//marks might not be of any use but still.
+	private int negativeMarks;
+	private String category;
+	private String question;
+	private String optionA;
+	private String optionB;
+	private String optionC;
+	private String optionD;
+	private String correctAnswer;
+	
+	public Question(int questionId, int level, int marks, int negativeMarks, String category, String question, String optionA, String optionB, String optionC, String optionD, String correctAnswer) {
+		// TODO Auto-generated constructor stub
+		this.questionId=questionId;
+		this.level=level;
+		this.marks=marks;
+		this.negativeMarks=negativeMarks;
+		this.category=category;
+		this.question=question;
+		this.optionA=optionA;
+		this.optionB=optionB;
+		this.optionC=optionC;
+		this.optionD=optionD;
+		this.correctAnswer=correctAnswer;
+	}
+
+	public int getQuestionId(){
+		return this.questionId;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public int getNegativeMarks() {
+		return negativeMarks;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String getOptionA() {
+		return optionA;
+	}
+
+	public String getOptionB() {
+		return optionB;
+	}
+
+	public String getOptionC() {
+		return optionC;
+	}
+
+	public String getOptionD() {
+		return optionD;
+	}
+
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+
+}
--- cadet/src/org/cadet/util/model/Constants.java
+++ cadet/src/org/cadet/util/model/Constants.java
@@ -99,7 +99,7 @@ public class Constants {
 	
 	/* Unknowm */
 	public static final String getPasswordClient = "SELECT Password FROM candidate WHERE CUserName = ?";
-	public static final String getQuestionCountOfCategoryOfTest="SELECT CategoryId, QuestionPerCategory FROM test WHERE testId = ?";
+	public static final String getQuestionCountAndCategoryOfTest="SELECT tc.CategoryId, c.CategoryName, tc.TimePerCategory, tc.QuestionPerCategory FROM testcategory tc JOIN category c on tc.CategoryId = c.CategoryId WHERE testId = ?";
 	public static final String fetchNextQuestion1="SELECT q.QuestionId, q.LevelId, c.CategoryName, q.Question, q.OptA, q.OptB, q.OptC, q.OptD, q.CorrectAnswer, l.Marks, t.NegMark FROM testquestion t JOIN questionbank q ON t.QuestionId=q.QuestionId JOIN levelmarks l ON t.TestId=l.TestId JOIN category c ON q.CategoryId=c.CategoryId WHERE q.LevelId=l.LevelId AND t.TestId= ? AND q.CategoryId= ? AND l.LevelId= ? AND q.QuestionId NOT IN (";
 	public static final String fetchNextQuestion2=") ORDER BY RAND() LIMIT 1";
 	public static final String saveResult="INSERT INTO result VALUES(?,?,?,?,?)";
