2882d05bede8967dd2d4ed90fdbee703e641f117
ucant2seeme <ucant2seeme@gmail.com>
2012-11-08 12:08:56 +0000

    main

--- cadet/WebContent/js/main.js
+++ cadet/WebContent/js/main.js
@@ -13,6 +13,15 @@ function submit_val(){
 	return true;
 }
 
+function submit_email(){
+	if(!validate2()){
+		return false;	
+	}
+	return true;
+}
+
+
+
 function hashmap(input){
 	var shaobj = new jsSHA(input,"ASCII");
 	var hash = shaobj.getHash("SHA-512", "HEX");
